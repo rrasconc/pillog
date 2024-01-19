4b823de0f018b79323fe6b4391446438a4b1efc0
rrasconc <rigobertorascon5@gmail.com>
2024-01-18 17:58:10 -0700

    feat: added new values to light theme

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -1,10 +1,16 @@
+import { scale } from 'react-native-size-matters'
+
 export const lightTheme = {
   colors: {
-    typography: '#000000',
+    primaryText: '#0F0F0F',
+    secondaryText: '#9CA2B1',
     background: '#ffffff',
+    primary: '#19B7A9',
+    secondary: '#F2F4F7',
   },
-  margins: {
-    sm: 2,
+  spacing: {
+    xs: scale(10),
+    sm: scale(14),
     md: 4,
     lg: 8,
     xl: 12,
