0c133deb7953a3061c24f254c49adfbaa82a2505
rrasconc <rigobertorascon5@gmail.com>
2024-02-01 19:35:47 -0700

    feat: added Pill Card component

---
+++ src/components/Pill.Card/index.ts
@@ -0,0 +1,1 @@
+export { default as PillCard } from './Pill.Card'
---
+++ src/components/Pill.Card/styles.ts
@@ -0,0 +1,15 @@
+import { createStyleSheet } from 'react-native-unistyles'
+
+export const stylesheet = createStyleSheet((theme) => ({
+  container: {
+    borderWidth: 1,
+    borderColor: theme.colors.border,
+    borderRadius: theme.spacing.xs,
+    padding: theme.spacing.sm,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  check: {
+    marginLeft: 'auto',
+  },
+}))
