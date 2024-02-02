ef9354df9b9e59646a5fe2ff9f9bf328f46b1f10
rrasconc <rigobertorascon5@gmail.com>
2024-02-02 09:03:40 -0700

    chore: added dummy data for mocking

---
+++ src/constants/dummy.ts
@@ -0,0 +1,16 @@
+import { Pill } from './types'
+
+export const PILLS: Pill[] = [
+  {
+    id: '7611d0ba-7e84-4f87-939c-de24ccca377a',
+    name: 'Amoxilina',
+    dose: 500,
+    doseType: 'mg',
+  },
+  {
+    id: '2a08612e-c548-4af8-80c9-ab55d1b697b7',
+    name: 'Paracetamol',
+    dose: 500,
+    doseType: 'mg',
+  },
+]
