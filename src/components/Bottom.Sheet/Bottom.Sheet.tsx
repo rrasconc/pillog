1655559c1c48e2732955cc9584a78326740b2ee4
rrasconc <rigobertorascon5@gmail.com>
2024-02-04 12:15:44 -0700

    feat: changed BottomSheet to BottomSheetModal with Backdrop

--- src/components/Bottom.Sheet/Bottom.Sheet.tsx
+++ src/components/Bottom.Sheet/Bottom.Sheet.tsx
@@ -1,30 +1,42 @@
-import DefaultBottomSheet from '@gorhom/bottom-sheet'
-import { View, Pressable } from 'react-native'
+import {
+  BottomSheetBackdrop,
+  BottomSheetBackdropProps,
+  BottomSheetModal,
+} from '@gorhom/bottom-sheet'
+import { forwardRef, useCallback } from 'react'
+import { View } from 'react-native'
 import { useStyles } from 'react-native-unistyles'
 
 import { stylesheet } from './styles'
 import { BottomSheetProps } from './types'
 import { Text } from '../Text'
 
-export default function BottomSheet({
-  onDismiss,
-  title,
-  children,
-  ...otherProps
-}: BottomSheetProps) {
+const BottomSheet = forwardRef(function BottomSheet(props: BottomSheetProps, ref: any) {
+  const { onDismiss, title, children, ...otherProps } = props
   const { styles } = useStyles(stylesheet)
-  return (
-    <>
-      <Pressable onPress={onDismiss} style={styles.mask} />
-
-      <DefaultBottomSheet handleIndicatorStyle={styles.handleIndicator} {...otherProps}>
-        <View style={styles.container}>
-          <View style={styles.titleContainer}>
-            <Text.Primary>{title}</Text.Primary>
-          </View>
-          <View style={styles.contentContainer}>{children}</View>
-        </View>
-      </DefaultBottomSheet>
-    </>
+  const renderBackdrop = useCallback(
+    (props: BottomSheetBackdropProps) => (
+      <BottomSheetBackdrop {...props} appearsOnIndex={0} disappearsOnIndex={-1} />
+    ),
+    [],
   )
-}
+
+  return (
+    <BottomSheetModal
+      ref={ref}
+      enablePanDownToClose
+      onClose={onDismiss}
+      backdropComponent={renderBackdrop}
+      handleIndicatorStyle={styles.handleIndicator}
+      {...otherProps}>
+      <View style={styles.container}>
+        <View style={styles.titleContainer}>
+          <Text.Primary>{title}</Text.Primary>
+        </View>
+        <View style={styles.contentContainer}>{children}</View>
+      </View>
+    </BottomSheetModal>
+  )
+})
+
+export default BottomSheet
