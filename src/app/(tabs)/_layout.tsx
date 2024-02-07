07e0576654e1581c2b0b83e2b22dee25f7c13f96
rrasconc <rigobertorascon5@gmail.com>
2024-02-06 18:41:02 -0700

    feat: added Log schema

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,71 +1,76 @@
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
+import { RealmProvider } from '@realm/react'
 import { Tabs } from 'expo-router/tabs'
 import { scale, verticalScale } from 'react-native-size-matters'
 import { createStyleSheet, useStyles } from 'react-native-unistyles'
 
 import { Icon } from '@/components/Icon'
+import { LogSchema, PillSchema } from '@/utils/models'
+
 export default function TabsLayout() {
   const { styles } = useStyles(stylesheet)
   const height = verticalScale(40)
 
   return (
-    <BottomSheetModalProvider>
-      <Tabs
-        screenOptions={{
-          headerShown: false,
-          tabBarStyle: {
-            height,
-          },
-          tabBarItemStyle: {
-            height,
-          },
-        }}>
-        <Tabs.Screen
-          name="home"
-          options={{
-            tabBarShowLabel: false,
-            tabBarIcon: ({ focused }) => (
-              <Icon
-                name="home-outline"
-                size={scale(22)}
-                style={focused ? styles.focusedIcon : styles.unfocusedIcon}
-              />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="logs"
-          options={{
-            tabBarShowLabel: false,
-            tabBarIcon: ({ focused }) => (
-              <Icon
-                name="time-outline"
-                style={focused ? styles.focusedIcon : styles.unfocusedIcon}
-              />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="settings"
-          options={{
-            tabBarShowLabel: false,
-            tabBarIcon: ({ focused }) => (
-              <Icon
-                name="settings-outline"
-                size={scale(22)}
-                style={focused ? styles.focusedIcon : styles.unfocusedIcon}
-              />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="index"
-          options={{
-            href: null,
-          }}
-        />
-      </Tabs>
-    </BottomSheetModalProvider>
+    <RealmProvider schema={[PillSchema, LogSchema]}>
+      <BottomSheetModalProvider>
+        <Tabs
+          screenOptions={{
+            headerShown: false,
+            tabBarStyle: {
+              height,
+            },
+            tabBarItemStyle: {
+              height,
+            },
+          }}>
+          <Tabs.Screen
+            name="home"
+            options={{
+              tabBarShowLabel: false,
+              tabBarIcon: ({ focused }) => (
+                <Icon
+                  name="home-outline"
+                  size={scale(22)}
+                  style={focused ? styles.focusedIcon : styles.unfocusedIcon}
+                />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="logs"
+            options={{
+              tabBarShowLabel: false,
+              tabBarIcon: ({ focused }) => (
+                <Icon
+                  name="time-outline"
+                  style={focused ? styles.focusedIcon : styles.unfocusedIcon}
+                />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="settings"
+            options={{
+              tabBarShowLabel: false,
+              tabBarIcon: ({ focused }) => (
+                <Icon
+                  name="settings-outline"
+                  size={scale(22)}
+                  style={focused ? styles.focusedIcon : styles.unfocusedIcon}
+                />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="index"
+            options={{
+              href: null,
+            }}
+          />
+        </Tabs>
+      </BottomSheetModalProvider>
+    </RealmProvider>
   )
 }
 
--- src/app/(tabs)/home/_layout.tsx
+++ src/app/(tabs)/home/_layout.tsx
@@ -1,32 +1,27 @@
 import 'react-native-get-random-values'
 import '@/config/unistyle'
-import { RealmProvider } from '@realm/react'
 import { Stack } from 'expo-router'
-
-import { PillSchema } from '@/utils/models'
 
 export default function Layout() {
   return (
-    <RealmProvider schema={[PillSchema]}>
-      <Stack
-        screenOptions={{
-          headerShadowVisible: false,
-        }}>
-        <Stack.Screen
-          name="index"
-          options={{
-            title: 'My pills',
-            presentation: 'modal',
-          }}
-        />
-        <Stack.Screen
-          name="new_pill"
-          options={{
-            title: 'Add a pill',
-            presentation: 'modal',
-          }}
-        />
-      </Stack>
-    </RealmProvider>
+    <Stack
+      screenOptions={{
+        headerShadowVisible: false,
+      }}>
+      <Stack.Screen
+        name="index"
+        options={{
+          title: 'My pills',
+          presentation: 'modal',
+        }}
+      />
+      <Stack.Screen
+        name="new_pill"
+        options={{
+          title: 'Add a pill',
+          presentation: 'modal',
+        }}
+      />
+    </Stack>
   )
 }
