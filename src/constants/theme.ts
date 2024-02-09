e1315c008536a4f73940702991f0f03d8dcac552
rrasconc <rigobertorascon5@gmail.com>
2024-02-09 12:29:05 -0700

    feat: add dark mode theme

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -1,21 +1,39 @@
 import { scale } from 'react-native-size-matters'
 
+const spacing = {
+  xs: scale(10),
+  sm: scale(14),
+  md: scale(18),
+  lg: scale(22),
+  xl: scale(26),
+}
+
 export const lightTheme = {
   colors: {
     primaryText: '#0F0F0F',
     secondaryText: '#9CA2B1',
     background: '#ffffff',
     primary: '#00AFFA',
+    transparentPrimary: 'rgba(0,175,250,0.2)',
     secondary: '#F2F4F7',
     border: '#EDEDED',
     success: '#19B7A9',
     danger: '#F90707',
   },
-  spacing: {
-    xs: scale(10),
-    sm: scale(14),
-    md: 4,
-    lg: 8,
-    xl: 12,
+  spacing,
+} as const
+
+export const darkTheme = {
+  colors: {
+    primaryText: '#ffffff',
+    secondaryText: '#9CA2B1',
+    background: '#000000',
+    primary: '#00AFFA',
+    transparentPrimary: 'rgba(0,175,250,0.3)',
+    secondary: '#151516',
+    border: '#333333',
+    success: '#19B7A9',
+    danger: '#F90707',
   },
+  spacing,
 } as const
