9cec8dd7dcdc0e0e59bd7b315957b81230630139
rrasconc <rigobertorascon5@gmail.com>
2024-03-02 11:13:50 -0700

    feat: add BaseEmptyState component

--- src/components/Base.Empty.State/Base.Empty.State.tsx
+++ src/components/Base.Empty.State/Base.Empty.State.tsx
@@ -1,17 +1,15 @@
 import { View } from 'react-native'
-import { scale } from 'react-native-size-matters'
 import { useStyles } from 'react-native-unistyles'
 
 import { stylesheet } from './styles'
 import type { GenericEmptyStateProps } from './types'
-import { Icon } from '../Icon'
 import { Text } from '../Text'
 
-export default function GenericEmptyState({ message }: GenericEmptyStateProps) {
+export default function BaseEmptyState({ message, iconComponent }: GenericEmptyStateProps) {
   const { theme, styles } = useStyles(stylesheet)
   return (
     <View style={styles.container}>
-      <Icon name="file-tray" size={scale(46)} color={theme.colors.placeholder} />
+      {iconComponent}
       <Text style={{ color: theme.colors.placeholder }}>{message}</Text>
     </View>
   )
--- src/components/Generic.Empty.State/types.d.ts
+++
@@ -1,3 +0,0 @@
-export interface GenericEmptyStateProps {
-  message?: string
-}
